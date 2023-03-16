dba44666f5f1d07f5e3c1c034ef8a76b5f94446d
Gabriella Vieira <gabvi2015@outlook.com>
2023-03-16 18:30:21 -0300

    Requisito 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
-// import PropTypes from 'prop-types';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends Component {
+  constructor() {
+    super();
+    this.state = {
+      artistName: '',
+      collectionName: '',
+      artImage: '',
+      songsList: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getMusicsList();
+  }
+
+  getMusicsList = async () => {
+    const { match } = this.props;
+    const { id } = match.params;
+    const musicListArray = await getMusics(id);
+    this.setState({
+      artistName: musicListArray[0].artistName,
+      collectionName: musicListArray[0].collectionName,
+      artImage: musicListArray[0].artworkUrl100,
+      songsList: musicListArray.filter(({ kind }) => kind),
+    });
+    // console.log(musicListArray);
+    // console.log(this.state.songsList);
+  };
+
   render() {
+    const { artistName, collectionName, artImage, songsList } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        Album
+        <section>
+          <img src={ artImage } alt={ collectionName } />
+          <h2 data-testid="album-name">{ collectionName }</h2>
+          <h4 data-testid="artist-name">{ artistName }</h4>
+        </section>
+        <section>
+          {songsList.map(({ trackId, trackName, previewUrl }) => (<MusicCard
+            key={ trackId }
+            trackName={ trackName }
+            previewUrl={ previewUrl }
+          />))}
+        </section>
       </div>
     );
   }
 }
 
-// Album.propTypes = {
-
-// };
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default Album;
